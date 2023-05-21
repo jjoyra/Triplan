fcead398e6c0ba6b995630daefa81490674d86ea
Im-hass <huiseon4107@gmail.com>
2023-05-22 00:37:45 +0900

    [FE] fix: 코드 포맷 변경

--- Frontend/TriPlan/src/components/common/TheHeader.vue
+++ Frontend/TriPlan/src/components/common/TheHeader.vue
@@ -1,117 +1,125 @@
 <template>
-<div>
   <div>
-    <div :class="{menuModal: isMenuOpen, closeMenuModal: !isMenuOpen}">
-      <div class="side-menu-wrap">
-        <router-link to="/"><img id="side-logo" src="../../assets/logo.png" alt="logo.png" /></router-link>
-        <div>
-          <b-icon icon="Bell"></b-icon>
-          <b-nav-item to="/notice" exact exact-active-class="active">공지사항</b-nav-item>
-        </div>
-        <div>
-          <b-icon icon="GeoAlt"></b-icon>
-          <b-nav-item to="/attraction" exact exact-active-class="active">관광지 찾기</b-nav-item>
-        </div>
-        <div>
-          <b-icon icon="ChatLeftDots"></b-icon>
-          <b-nav-item to="/community" exact exact-active-class="active">커뮤니티</b-nav-item>
-        </div>
-        <div>
-          <b-icon icon="CardText"></b-icon>
-          <b-nav-item to="/review" exact exact-active-class="active">플랜리뷰</b-nav-item>
-        </div>
-        
-        <div class="profile-wrap">
-          <img id="profileImg" src="../../assets/300.png" alt="300.png" />
-          <b-nav-item-dropdown
-            id="profile-dropdown"
-            text="임하스"
-            toggle-class="nav-link-custom"
-            right
-          >
-            <b-dropdown-item>
-              <b-nav-item to="/mypage" exact exact-active-class="active">마이페이지</b-nav-item>
-            </b-dropdown-item>
-            <b-dropdown-divider></b-dropdown-divider>
-            <b-dropdown-item>
-              <b-nav-item to="/plan/write" exact exact-active-class="active">플랜 만들기</b-nav-item>
-            </b-dropdown-item>
-            <b-dropdown-item>
-              <b-nav-item to="/review" exact exact-active-class="active">로그아웃</b-nav-item>
-            </b-dropdown-item>
-          </b-nav-item-dropdown>
-        </div>
-      </div>
-    </div>
-    <div :class="{overlay: isMenuOpen}" @click="handleMenuClose"></div>
-  </div>
-  <div class="header-wrap">
-    <b-nav pills>
-      <div class="logo-search-wrap">
-        <div class="logo-menu-wrap">
-          <b-icon class="list-icon" icon="List" v-on:click="handleMenu"></b-icon>
-          <router-link to="/"><img id="logo" src="../../assets/logo.png" alt="logo.png" /></router-link>
-        </div>
-        <div :class="{searchWrap: true, searchDisplay: !isMain}">
-          <search-input placeholderTxt="여행지를 찾아보세요"></search-input>
-        </div>
-      </div>
+    <div>
+      <div :class="{ menuModal: isMenuOpen, closeMenuModal: !isMenuOpen }">
+        <div class="side-menu-wrap">
+          <router-link to="/"
+            ><img id="side-logo" src="../../assets/logo.png" alt="logo.png"
+          /></router-link>
+          <div>
+            <b-icon icon="Bell"></b-icon>
+            <b-nav-item to="/notice" exact exact-active-class="active">공지사항</b-nav-item>
+          </div>
+          <div>
+            <b-icon icon="GeoAlt"></b-icon>
+            <b-nav-item to="/attraction" exact exact-active-class="active">관광지 찾기</b-nav-item>
+          </div>
+          <div>
+            <b-icon icon="ChatLeftDots"></b-icon>
+            <b-nav-item to="/community" exact exact-active-class="active">커뮤니티</b-nav-item>
+          </div>
+          <div>
+            <b-icon icon="CardText"></b-icon>
+            <b-nav-item to="/review" exact exact-active-class="active">플랜리뷰</b-nav-item>
+          </div>
 
-      <div class="menu-wrap">
-        <div>
-          <b-icon icon="Bell"></b-icon>
-          <b-nav-item to="/notice" exact exact-active-class="active">공지사항</b-nav-item>
-        </div>
-        <div>
-          <b-icon icon="GeoAlt"></b-icon>
-          <b-nav-item to="/attraction" exact exact-active-class="active">관광지 찾기</b-nav-item>
-        </div>
-        <div>
-          <b-icon icon="ChatLeftDots"></b-icon>
-          <b-nav-item to="/community" exact exact-active-class="active">커뮤니티</b-nav-item>
-        </div>
-        <div>
-          <b-icon icon="CardText"></b-icon>
-          <b-nav-item to="/review" exact exact-active-class="active">플랜리뷰</b-nav-item>
-        </div>
-        
-        <div class="profile-wrap">
-          <img id="profileImg" src="../../assets/300.png" alt="300.png" />
-          <b-nav-item-dropdown
-            id="profile-dropdown"
-            text="임하스"
-            toggle-class="nav-link-custom"
-            right
-          >
-            <b-dropdown-item>
-              <b-nav-item to="/mypage" exact exact-active-class="active">마이페이지</b-nav-item>
-            </b-dropdown-item>
-            <b-dropdown-divider></b-dropdown-divider>
-            <b-dropdown-item>
-              <b-nav-item to="/plan/write" exact exact-active-class="active">플랜 만들기</b-nav-item>
-            </b-dropdown-item>
-            <b-dropdown-item>
-              <b-nav-item to="/review" exact exact-active-class="active">로그아웃</b-nav-item>
-            </b-dropdown-item>
-          </b-nav-item-dropdown>
+          <div class="profile-wrap">
+            <img id="profileImg" src="../../assets/300.png" alt="300.png" />
+            <b-nav-item-dropdown
+              id="profile-dropdown"
+              text="임하스"
+              toggle-class="nav-link-custom"
+              right
+            >
+              <b-dropdown-item>
+                <b-nav-item to="/mypage" exact exact-active-class="active">마이페이지</b-nav-item>
+              </b-dropdown-item>
+              <b-dropdown-divider></b-dropdown-divider>
+              <b-dropdown-item>
+                <b-nav-item to="/plan/write" exact exact-active-class="active"
+                  >플랜 만들기</b-nav-item
+                >
+              </b-dropdown-item>
+              <b-dropdown-item>
+                <b-nav-item to="/review" exact exact-active-class="active">로그아웃</b-nav-item>
+              </b-dropdown-item>
+            </b-nav-item-dropdown>
+          </div>
         </div>
       </div>
-    </b-nav>
+      <div :class="{ overlay: isMenuOpen }" @click="handleMenuClose"></div>
+    </div>
+    <div class="header-wrap">
+      <b-nav pills>
+        <div class="logo-search-wrap">
+          <div class="logo-menu-wrap">
+            <b-icon class="list-icon" icon="List" v-on:click="handleMenu"></b-icon>
+            <router-link to="/"
+              ><img id="logo" src="../../assets/logo.png" alt="logo.png"
+            /></router-link>
+          </div>
+          <div :class="{ searchWrap: true, searchDisplay: !isMain }">
+            <search-input placeholderTxt="여행지를 찾아보세요"></search-input>
+          </div>
+        </div>
+
+        <div class="menu-wrap">
+          <div>
+            <b-icon icon="Bell"></b-icon>
+            <b-nav-item to="/notice" exact exact-active-class="active">공지사항</b-nav-item>
+          </div>
+          <div>
+            <b-icon icon="GeoAlt"></b-icon>
+            <b-nav-item to="/attraction" exact exact-active-class="active">관광지 찾기</b-nav-item>
+          </div>
+          <div>
+            <b-icon icon="ChatLeftDots"></b-icon>
+            <b-nav-item to="/community" exact exact-active-class="active">커뮤니티</b-nav-item>
+          </div>
+          <div>
+            <b-icon icon="CardText"></b-icon>
+            <b-nav-item to="/review" exact exact-active-class="active">플랜리뷰</b-nav-item>
+          </div>
+
+          <div class="profile-wrap">
+            <img id="profileImg" src="../../assets/300.png" alt="300.png" />
+            <b-nav-item-dropdown
+              id="profile-dropdown"
+              text="임하스"
+              toggle-class="nav-link-custom"
+              right
+            >
+              <b-dropdown-item>
+                <b-nav-item to="/mypage" exact exact-active-class="active">마이페이지</b-nav-item>
+              </b-dropdown-item>
+              <b-dropdown-divider></b-dropdown-divider>
+              <b-dropdown-item>
+                <b-nav-item to="/plan/write" exact exact-active-class="active"
+                  >플랜 만들기</b-nav-item
+                >
+              </b-dropdown-item>
+              <b-dropdown-item>
+                <b-nav-item to="/review" exact exact-active-class="active">로그아웃</b-nav-item>
+              </b-dropdown-item>
+            </b-nav-item-dropdown>
+          </div>
+        </div>
+      </b-nav>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
-import SearchInput from '@/components/common/SearchInput.vue';
+import SearchInput from "@/components/common/SearchInput.vue";
 
 export default {
-  name: 'TheHeader',
+  name: "TheHeader",
   data() {
     return {
       width: 0,
       isMenuOpen: false,
       isMain: true,
-    }
+    };
   },
   methods: {
     handleMenu() {
@@ -122,47 +130,47 @@ export default {
     },
     handleResize() {
       this.width = window.innerWidth;
-    }
+    },
   },
   components: {
     SearchInput,
   },
   watch: {
-    '$route' (to) {
-      if (to.fullPath === '/') {
+    $route(to) {
+      if (to.fullPath === "/") {
         this.isMain = true;
-      } else if (this.$route.fullPath !== '/' && this.width < 950) {
+      } else if (this.$route.fullPath !== "/" && this.width < 950) {
         this.isMain = false;
       } else {
         this.isMain = true;
       }
       this.isMenuOpen = false;
     },
-    width (val) {
-      if (this.$route.fullPath === '/') {
+    width(val) {
+      if (this.$route.fullPath === "/") {
         this.isMain = true;
-      } else if (this.$route.fullPath !== '/' && val > 950) {
+      } else if (this.$route.fullPath !== "/" && val > 950) {
         this.isMain = true;
       } else {
         this.isMain = false;
       }
       this.isMenuOpen = false;
-    }
+    },
   },
   mounted() {
-    window.addEventListener('resize', this.handleResize);
+    window.addEventListener("resize", this.handleResize);
     this.width = window.innerWidth;
-	},
-  beforeDestroy() {
-    window.removeEventListener('resize', this.handleResize);
   },
-}
+  beforeDestroy() {
+    window.removeEventListener("resize", this.handleResize);
+  },
+};
 </script>
 
 <style scope>
 .header-wrap {
   padding: 10px 30px;
-  margin : 0 auto;
+  margin: 0 auto;
   max-width: 1280px;
 }
 
@@ -175,7 +183,7 @@ export default {
 
 .nav a {
   font-size: 1rem;
-  color: #343A40;
+  color: #343a40;
   flex-wrap: wrap;
 }
 
@@ -193,7 +201,7 @@ export default {
 }
 .logo-search-wrap #logo {
   margin-right: 1rem;
-  height: calc(1.5em + .75rem - 2px);
+  height: calc(1.5em + 0.75rem - 2px);
 }
 .logo-search-wrap .logo-menu-wrap .list-icon {
   display: none;
@@ -214,9 +222,10 @@ export default {
   border-radius: 50%;
 }
 
-.nav-pills .nav-link.active, .nav-pills .show>.nav-link {
+.nav-pills .nav-link.active,
+.nav-pills .show > .nav-link {
   background: transparent;
-  color: #343A40;
+  color: #343a40;
 }
 
 .closeMenuModal .side-menu-wrap,
@@ -241,7 +250,7 @@ export default {
 
   .menuModal .side-menu-wrap #side-logo {
     margin: 0 auto;
-    height: calc(1.5em + .75rem - 2px);
+    height: calc(1.5em + 0.75rem - 2px);
     display: block;
   }
 
@@ -264,7 +273,6 @@ export default {
     padding-left: 1.25rem;
   }
 
-
   .closeMenuModal,
   .menuModal,
   .overlay {
@@ -278,7 +286,7 @@ export default {
   .closeMenuModal,
   .menuModal {
     width: 70%;
-    background-color: #FFF;
+    background-color: #fff;
     z-index: 12;
     transition: left 0.5s;
   }
@@ -290,7 +298,7 @@ export default {
   .closeMenuModal {
     left: -70%;
   }
-  
+
   .overlay {
     opacity: 0.5;
     background-color: #000;
@@ -347,7 +355,5 @@ export default {
   .menu-wrap {
     display: none;
   }
-  
 }
-
 </style>
--- Frontend/TriPlan/src/views/MainView.vue
+++ Frontend/TriPlan/src/views/MainView.vue
@@ -2,73 +2,71 @@
   <div>
     <b-container fluid>
       <b-carousel
-      id="carousel-1"
-      v-model="slide"
-      :interval="4000"
-      controls
-      indicators
-      background="#ababab"
-      img-width="1024"
-      img-height="480"
-      style="text-shadow: 1px 1px 2px #333;"
-      @sliding-start="onSlideStart"
-      @sliding-end="onSlideEnd"
-    >
-      <!-- Text slides with image -->
-      <b-carousel-slide
-        caption="First slide"
-        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-        img-src="https://picsum.photos/1024/480/?image=52"
-      ></b-carousel-slide>
+        id="carousel-1"
+        v-model="slide"
+        :interval="4000"
+        controls
+        indicators
+        background="#ababab"
+        img-width="1024"
+        img-height="480"
+        style="text-shadow: 1px 1px 2px #333"
+        @sliding-start="onSlideStart"
+        @sliding-end="onSlideEnd"
+      >
+        <!-- Text slides with image -->
+        <b-carousel-slide
+          caption="First slide"
+          text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+          img-src="https://picsum.photos/1024/480/?image=52"
+        ></b-carousel-slide>
 
-      <!-- Slides with custom text -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
-        <h1>Hello world!</h1>
-      </b-carousel-slide>
+        <!-- Slides with custom text -->
+        <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+          <h1>Hello world!</h1>
+        </b-carousel-slide>
 
-      <!-- Slides with image only -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
+        <!-- Slides with image only -->
+        <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
 
-      <!-- Slides with img slot -->
-      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-      <b-carousel-slide>
-        <template #img>
-          <img
-            class="d-block img-fluid w-100"
-            width="1024"
-            height="480"
-            src="https://picsum.photos/1024/480/?image=55"
-            alt="image slot"
-          >
-        </template>
-      </b-carousel-slide>
+        <!-- Slides with img slot -->
+        <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+        <b-carousel-slide>
+          <template #img>
+            <img
+              class="d-block img-fluid w-100"
+              width="1024"
+              height="480"
+              src="https://picsum.photos/1024/480/?image=55"
+              alt="image slot"
+            />
+          </template>
+        </b-carousel-slide>
 
-      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
-          a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
-        </p>
-      </b-carousel-slide>
-    </b-carousel>
+        <!-- Slide with blank fluid image to maintain slide aspect ratio -->
+        <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis,
+            tincidunt a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
+          </p>
+        </b-carousel-slide>
+      </b-carousel>
     </b-container>
-    <b-container>
-      Main View 입니다.
-    </b-container>
+    <b-container> Main View 입니다. </b-container>
   </div>
 </template>
 
 <script>
-import axios from '@/api/http';
+import axios from "@/api/http";
 
 export default {
   name: "MainView",
   created() {
-    axios.get('notice').then(res => {
+    axios.get("notice").then((res) => {
       console.log(res);
-    })
-  }
-}
+    });
+  },
+};
 </script>
 
 <style scoped>
