90c8728c456d1a365de7af7dd11bfc35be19bd77
Im-hass <huiseon4107@gmail.com>
2023-05-24 17:47:15 +0900

    [FE] fix: review api 비동기 처리 추가

--- Frontend/TriPlan/src/api/notice.js
+++ Frontend/TriPlan/src/api/notice.js
@@ -2,27 +2,27 @@ import { apiInstance } from "./http";
 
 const api = apiInstance();
 
-function registNotice(notice, success, fail) {
-  api.post(`/notice`, notice).then(success).catch(fail);
+async function registNotice(notice, success, fail) {
+  await api.post(`/notice`, notice).then(success).catch(fail);
 }
 
-function getNoticeList(param, success, fail) {
-  api.get(`/notice`, { params: param }).then(success).catch(fail);
+async function getNoticeList(param, success, fail) {
+  await api.get(`/notice`, { params: param }).then(success).catch(fail);
 }
 
-function modifyNotice(notice, success, fail) {
-  api.put(`/notice`, notice).then(success).catch(fail);
+async function modifyNotice(notice, success, fail) {
+  await api.put(`/notice`, notice).then(success).catch(fail);
 }
 
-function deleteNotice(noticeid, success, fail) {
-  api.put(`/notice/${noticeid}`).then(success).catch(fail);
+async function deleteNotice(noticeid, success, fail) {
+  await api.put(`/notice/${noticeid}`).then(success).catch(fail);
 }
 
-function getNoticeDetail(noticeid, success, fail) {
-  api.get(`/notice/${noticeid}`).then(success).catch(fail);
+async function getNoticeDetail(noticeid, success, fail) {
+  await api.get(`/notice/${noticeid}`).then(success).catch(fail);
 }
 
-function getTotalNoticeCount(param, success, fail) {
+async function getTotalNoticeCount(param, success, fail) {
   api.get(`/notice/count`, { params: param }).then(success).catch(fail);
 }
 
